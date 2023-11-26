51465e85b27e62ff499dbf7515c9a99cb02f2eaf
PiotrWolnik <w.peter@interia.eu>
2023-11-26 16:47:24 +0100

    Fix error

--- web.py
+++ web.py
@@ -141,9 +141,8 @@ wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     st.audio(wav_audio_data, format='audio/wav')
-
-with open('audio.wav', mode='wb') as f:
-    f.write(wav_audio_data)
+    with open('audio.wav', mode='wb') as f:
+        f.write(wav_audio_data)
 
 if center_column.button("Translate"):
     model = whisper.load_model("base")
