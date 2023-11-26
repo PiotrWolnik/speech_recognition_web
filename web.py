7315f0b51e51216926347a500aba00abd9afb05f
PiotrWolnik <w.peter@interia.eu>
2023-11-26 16:59:36 +0100

    Check sth one more time

--- web.py
+++ web.py
@@ -140,7 +140,7 @@ destination_language = center_column.selectbox(
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
+    # st.audio(wav_audio_data, format='audio/wav')
     with open('audio.wav', mode='wb') as f:
         f.write(wav_audio_data)
 
