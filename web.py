96c6929d0815049d582ded4ab73c473c879c6382
PiotrWolnik <w.peter@interia.eu>
2023-12-28 20:33:58 +0100

    Check sth

--- web.py
+++ web.py
@@ -1,87 +1,3 @@
-import wave
-from dataclasses import dataclass, asdict
-
-import pyaudio
-
-
-@dataclass
-class StreamParams:
-    format: int = pyaudio.paInt16
-    channels: int = 2
-    rate: int = 44100
-    frames_per_buffer: int = 1024
-    input: bool = True
-    output: bool = False
-
-    def to_dict(self) -> dict:
-        return asdict(self)
-
-
-class Recorder:
-    """Recorder uses the blocking I/O facility from pyaudio to record sound
-    from mic.
-
-    Attributes:
-        - stream_params: StreamParams object with values for pyaudio Stream
-            object
-    """
-    def __init__(self, stream_params: StreamParams) -> None:
-        self.stream_params = stream_params
-        self._pyaudio = None
-        self._stream = None
-        self._wav_file = None
-
-    def record(self, duration: int, save_path: str) -> None:
-        """Record sound from mic for a given amount of seconds.
-
-        :param duration: Number of seconds we want to record for
-        :param save_path: Where to store recording
-        """
-        print("Start recording...")
-        self._create_recording_resources(save_path)
-        self._write_wav_file_reading_from_stream(duration)
-        self._close_recording_resources()
-        print("Stop recording")
-
-    def _create_recording_resources(self, save_path: str) -> None:
-        self._pyaudio = pyaudio.PyAudio()
-        self._stream = self._pyaudio.open(**self.stream_params.to_dict())
-        self._create_wav_file(save_path)
-
-    def _create_wav_file(self, save_path: str):
-        self._wav_file = wave.open(save_path, "wb")
-        self._wav_file.setnchannels(self.stream_params.channels)
-        self._wav_file.setsampwidth(self._pyaudio.get_sample_size(self.stream_params.format))
-        self._wav_file.setframerate(self.stream_params.rate)
-
-    def _write_wav_file_reading_from_stream(self, duration: int) -> None:
-        for _ in range(int(self.stream_params.rate * duration / self.stream_params.frames_per_buffer)):
-            audio_data = self._stream.read(self.stream_params.frames_per_buffer)
-            self._wav_file.writeframes(audio_data)
-
-    def _close_recording_resources(self) -> None:
-        self._wav_file.close()
-        self._stream.close()
-        self._pyaudio.terminate()
-
-# import streamlit as st
-# from bokeh.models.widgets import Button
-# from bokeh.models import CustomJS
-
-# text = st.text_input("Say what ?")
-
-# tts_button = Button(label="Speak", width=100)
-
-# tts_button.js_on_event("button_click", CustomJS(code=f"""
-#     var u = new SpeechSynthesisUtterance();
-#     u.text = "{text}";
-#     u.lang = 'en-US';
-
-#     speechSynthesis.speak(u);
-#     """))
-
-# st.bokeh_chart(tts_button)
-
 import streamlit as st
 from languages import supported_languages
 from abc import ABC, abstractmethod
@@ -90,17 +6,8 @@ import speech_recognition as sr
 import cython
 import whisper
 from st_audiorec import st_audiorec
-import torch
 
-class ITranslateWords(ABC):
-    def __init__(self):
-        super().__init__()
-
-    @abstractmethod
-    def getResult(self) -> str:
-        pass
-
-class TranslateWords(ITranslateWords):
+class TranslateWords:
     def __init__(self, text_to_translate: str, language_to_translate_to: str):
         super().__init__()
         self.text_to_translate = text_to_translate
@@ -109,6 +16,10 @@ class TranslateWords(ITranslateWords):
                                 target=self.language_to_translate_to).translate(self.text_to_translate)
     def getResult(self) -> str:
         return self.result
+
+class TranslateSpeech:
+    def __init__(self) -> None:
+        pass
 
 st.set_page_config(page_title="Speech_Translator_Option.ai", page_icon=":studio_microphone:")
 
@@ -154,12 +65,19 @@ def write_large_data_to_file_parallel(filename, data, num_processes=40):
     pool.join()
 
 wav_audio_data = st_audiorec()
+from scipy.io.wavfile import write
+import numpy as np
 
 if wav_audio_data is not None:
     f = open('audio.wav', 'r+')
     f.truncate(0)
+    samplerate = 44100; fs = 100
+    t = np.linspace(0., 1., samplerate)
+    amplitude = np.iinfo(np.int16).max
+    data = amplitude * np.sin(2. * np.pi * fs * t)
+    write("audio.wav", samplerate, data.astype(np.int16))
     # st.audio(wav_audio_data, format='audio/wav')
-    write_large_data_to_file_parallel("audio.wav", wav_audio_data)
+    # write_large_data_to_file_parallel("audio.wav", wav_audio_data)
     # with open('audio.wav', mode='wb') as f:
     #     f.write(wav_audio_data)
 
