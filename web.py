726dc60fef206f195f212d4c1d1c9fca2bc1c01c
PiotrWolnik <w.peter@interia.eu>
2023-12-28 20:42:00 +0100

    Change library for recording audio

--- web.py
+++ web.py
@@ -35,51 +35,17 @@ destination_language = center_column.selectbox(
         label_visibility="hidden",
 )
 
-# if center_column.button("Click and say sth......"):
-#     stream_params = StreamParams()
-#     recorder = Recorder(stream_params)
-#     recorder.record(5, "audio.wav")
-    
-# if center_column.button("Translate"):
-#     model = whisper.load_model("base")
-#     transcription = model.transcribe("audio.wav")
-#     st.markdown(transcription["text"])
-#     st.markdown(TranslateWords(transcription["text"], supported_languages[destination_language]).getResult())
+from audiorecorder import audiorecorder
 
-# st.sidebar.header("Play Original Audio File")
-# st.sidebar.audio("audio.wav")
-import multiprocessing
- 
-def write_chunk_to_file(args):
-    filename, chunk = args
-    with open(filename, "ab") as file:
-        file.write(chunk)
- 
-def write_large_data_to_file_parallel(filename, data, num_processes=40):
-    pool = multiprocessing.Pool(processes=num_processes)
-    chunk_size = len(data) // num_processes
-    chunked_data = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
-    args_list = [(filename, chunk) for chunk in chunked_data]
-    pool.map(write_chunk_to_file, args_list)
-    pool.close()
-    pool.join()
+st.title("Audio Recorder")
+audio = audiorecorder("Click to record", "Click to stop recording")
 
-wav_audio_data = st_audiorec()
-from scipy.io.wavfile import write
-import numpy as np
+if len(audio) > 0:
+    # To play audio in frontend:
+    st.audio(audio.export().read())  
 
-if wav_audio_data is not None:
-    f = open('audio.wav', 'r+')
-    f.truncate(0)
-    samplerate = 44100; fs = 100
-    t = np.linspace(0., 1., samplerate)
-    amplitude = np.iinfo(np.int16).max
-    data = amplitude * np.sin(2. * np.pi * fs * t)
-    write("audio.wav", samplerate, data.astype(np.int16))
-    # st.audio(wav_audio_data, format='audio/wav')
-    # write_large_data_to_file_parallel("audio.wav", wav_audio_data)
-    # with open('audio.wav', mode='wb') as f:
-    #     f.write(wav_audio_data)
+    # To save audio to a file, use pydub export method:
+    audio.export("audio.wav", format="wav")
 
 if center_column.button("Translate"):
     model = whisper.load_model("base")
