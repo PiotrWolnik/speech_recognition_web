239be3e5d2008df1070e5c3d7a827a4eb7f6ef84
PiotrWolnik <w.peter@interia.eu>
2024-01-03 15:58:41 +0100

    Add option to choose source language instead of auto only

--- web.py
+++ web.py
@@ -8,11 +8,12 @@ from audiorecorder import audiorecorder
 import numpy as np
 
 class TranslateWords:
-    def __init__(self, text_to_translate: str, language_to_translate_to: str):
+    def __init__(self, text_to_translate: str, language_to_translate_to: str, source_language: str = 'auto'):
         super().__init__()
         self.text_to_translate = text_to_translate
         self.language_to_translate_to = language_to_translate_to
-        self.result = GoogleTranslator(source='auto', 
+        print(f"Inside translate words: {language_to_translate_to}")
+        self.result = GoogleTranslator(source=source_language, 
                                 target=self.language_to_translate_to).translate(self.text_to_translate)
     def getResult(self) -> str:
         return self.result
